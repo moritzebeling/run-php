24c949dc86bc6b379f9d7dc0fdf780e6410c65f1
moritzebeling <git@moritzebeling.com>
2021-02-01 00:38:49 +0100

    add basic theme

---
+++ theme/templates/default.php
@@ -0,0 +1,9 @@
+<?php snippet('header'); ?>
+
+Website
+
+<script>
+    window.siteData = <?php // echo json_encode( $data ); ?>;
+</script>
+
+<?php snippet('footer'); ?>
---
+++ theme/templates/snippets/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset='utf-8'>
+	<meta name='viewport' content='width=device-width,initial-scale=1'>
+
+	<title>30 Second Websites</title>
+
+	<?= css('styles.css') ?>
+	<?= js('bundle.js') ?>
+
+	<!--
+    <link rel="icon" type="image/svg+xml" href="/theme/favicon/favicon.svg">
+	<link rel="icon" type="image/png" href="/theme/favicon/favicon-128.png" sizes="128x128">
+	<link rel="icon" type="image/png" href="/theme/favicon/favicon-256.png" sizes="256x256">
+	<link rel="icon" type="image/png" href="/theme/favicon/favicon-512.png" sizes="512x512">
+	<link rel="icon" type="image/png" href="/theme/favicon/favicon-1024.png" sizes="1024x1024">
+    -->
+
+</head>
